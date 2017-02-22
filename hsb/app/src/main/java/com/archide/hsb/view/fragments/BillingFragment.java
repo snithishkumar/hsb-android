e7498a95400e5afee0310dd2bdbf7983ff3e7cef
Nithish <snithishkumar@gmail.com>
2017-02-23 00:31:05 +0530

    Show Billing Details

--- hsb/app/src/main/java/com/archide/hsb/view/fragments/BillingFragment.java
+++ hsb/app/src/main/java/com/archide/hsb/view/fragments/BillingFragment.java
@@ -11,6 +11,9 @@ import android.graphics.PorterDuffColorFilter;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,11 +27,17 @@ import com.archide.hsb.view.activities.ActivityUtil;
 import com.archide.hsb.view.activities.HomeActivity;
 import com.archide.hsb.view.activities.MainActivity;
 import com.archide.hsb.view.activities.NaviDrawerActivity;
+import com.archide.hsb.view.adapters.PlacedOrderHisMenuItemsAdapter;
 import com.archide.hsb.view.model.CloseOrderViewModel;
+import com.archide.hsb.view.model.MenuItemsViewModel;
+import com.archide.hsb.view.model.PlaceAnOrderViewModel;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import hsb.archide.com.hsb.R;
 
@@ -43,9 +52,9 @@ public class BillingFragment extends Fragment implements View.OnClickListener {
     TextView vTableNumber = null;
     TextView vTotalAmount = null;
     TextView vUserMobileNumber = null;
-    ImageView vOrderSyncStatus = null;
+    //ImageView vOrderSyncStatus = null;
 
-    TextView vOrderSyncMsg = null;
+   // TextView vOrderSyncMsg = null;
     Button vOrderLogOut = null;
     Button vOrderSent = null;
 
@@ -54,6 +63,17 @@ public class BillingFragment extends Fragment implements View.OnClickListener {
 
     private LayoutInflater mInflater;
     private ViewGroup mContainer;
+
+    TextView subTotalBeforeDiscount ;
+    TextView discount ;
+    TextView subTotal ;
+    TextView serviceTax ;
+    TextView totalAmount ;
+
+
+    LinearLayoutManager linearLayoutManager = null;
+    PlacedOrderHisMenuItemsAdapter orderedMenuItemsAdapter;
+    List<MenuItemsViewModel> menuItemsViewModels = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,8 +87,14 @@ public class BillingFragment extends Fragment implements View.OnClickListener {
         vTotalAmount =  (TextView) view.findViewById(R.id.close_order_total_amount);
         vUserMobileNumber =  (TextView) view.findViewById(R.id.close_order_mobile_number);
 
-        vOrderSyncStatus =  (ImageView) view.findViewById(R.id.close_order_sync_status);
-        vOrderSyncMsg =  (TextView) view.findViewById(R.id.close_order_sync_msg);
+        subTotalBeforeDiscount =  (TextView)view.findViewById(R.id.odr_his_subtotal_before_discount);
+        discount =  (TextView) view.findViewById(R.id.odr_his_discount);
+        subTotal =  (TextView) view.findViewById(R.id.odr_his_subtotal);
+        serviceTax =  (TextView) view.findViewById(R.id.odr_his_service_tax);
+        totalAmount =  (TextView) view.findViewById(R.id.odr_his_total_amount);
+
+       // vOrderSyncStatus =  (ImageView) view.findViewById(R.id.close_order_sync_status);
+        //vOrderSyncMsg =  (TextView) view.findViewById(R.id.close_order_sync_msg);
         vOrderLogOut =  (Button) view.findViewById(R.id.close_order_logout);
         vOrderSent =  (Button) view.findViewById(R.id.close_order_send);
 
@@ -90,8 +116,24 @@ public class BillingFragment extends Fragment implements View.OnClickListener {
         naviDrawerActivity.getSupportActionBar().setHomeButtonEnabled(false);
 
 
-        init(view);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.odr_his_order_data);
+        recyclerView.setHasFixedSize(true);
+        linearLayoutManager =  new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
 
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+
+        init(view);
+        setAdapters(recyclerView);
         getDataFromServer();
         return view;
     }
@@ -113,7 +155,7 @@ public class BillingFragment extends Fragment implements View.OnClickListener {
         Drawable mDrawable = naviDrawerActivity.getResources().getDrawable(R.drawable.ic_error_white_48dp);
         mDrawable.setColorFilter(new
                 PorterDuffColorFilter(Color.parseColor("#B71c1c"), PorterDuff.Mode.MULTIPLY));
-        vOrderSyncStatus.setImageDrawable(mDrawable);
+        //vOrderSyncStatus.setImageDrawable(mDrawable);
         vOrderLogOut.setVisibility(View.GONE);
         vOrderSent.setVisibility(View.VISIBLE);
     }
@@ -121,7 +163,7 @@ public class BillingFragment extends Fragment implements View.OnClickListener {
 
     private void showSuccess(){
         Drawable mDrawable = naviDrawerActivity.getResources().getDrawable(R.drawable.ic_error_white_48dp);
-        vOrderSyncStatus.setImageDrawable(mDrawable);
+        //vOrderSyncStatus.setImageDrawable(mDrawable);
 
         vOrderLogOut.setVisibility(View.VISIBLE);
         vOrderSent.setVisibility(View.GONE);
@@ -233,6 +275,7 @@ public class BillingFragment extends Fragment implements View.OnClickListener {
             case 400:
                 naviDrawerActivity.getOrderService().closeOrder(ActivityUtil.USER_MOBILE);
                 populateData();
+                populateOrderData();
                 naviDrawerActivity.setLogOut(true);
                 break;
           /*  case 405:
@@ -279,6 +322,27 @@ public class BillingFragment extends Fragment implements View.OnClickListener {
 
     }
 
+
+    private void setAdapters(RecyclerView recyclerView){
+        orderedMenuItemsAdapter = new PlacedOrderHisMenuItemsAdapter(menuItemsViewModels,naviDrawerActivity);
+        recyclerView.setAdapter(orderedMenuItemsAdapter);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(naviDrawerActivity,linearLayoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+    }
+
+    private void populateOrderData(){
+        PlaceAnOrderViewModel placeAnOrderViewModel =  naviDrawerActivity.getOrderService().getPlacedHistoryOrderViewModel();
+        menuItemsViewModels.clear();
+        menuItemsViewModels.addAll(placeAnOrderViewModel.getMenuItemsViewModels());
+        orderedMenuItemsAdapter.notifyDataSetChanged();
+
+
+        vOrderId.setText("Order Id:"+String.valueOf(placeAnOrderViewModel.getOrderId()));
+        subTotalBeforeDiscount.setText(getString(R.string.pound)+" "+String.valueOf(placeAnOrderViewModel.getSubTotalBeforeDiscount()));
+        discount.setText(getString(R.string.pound)+" "+String.valueOf(placeAnOrderViewModel.getDiscount()));
+        subTotal.setText(getString(R.string.pound)+" "+String.valueOf(placeAnOrderViewModel.getSubTotal()));
+        totalAmount.setText(getString(R.string.pound)+" "+String.valueOf(placeAnOrderViewModel.getTotalAmount()));
+    }
 
 
 
