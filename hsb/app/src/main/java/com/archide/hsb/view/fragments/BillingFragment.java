d1bf39365054a304a3d1fa129eebed4a25fdae22
Nithish <snithishkumar@gmail.com>
2017-03-16 14:51:14 +0530

    Text to voice added

--- hsb/app/src/main/java/com/archide/hsb/view/fragments/BillingFragment.java
+++ hsb/app/src/main/java/com/archide/hsb/view/fragments/BillingFragment.java
@@ -3,6 +3,7 @@ package com.archide.hsb.view.fragments;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -23,6 +24,7 @@ import com.archide.hsb.view.model.PlaceAnOrderViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import hsb.archide.com.hsb.R;
 
@@ -30,7 +32,7 @@ import hsb.archide.com.hsb.R;
  * Created by Nithish on 23/11/16.
  */
 
-public class BillingFragment extends Fragment implements View.OnClickListener {
+public class BillingFragment extends Fragment implements View.OnClickListener,TextToSpeech.OnInitListener  {
 
 
     TextView vOrderId = null;
@@ -59,10 +61,13 @@ public class BillingFragment extends Fragment implements View.OnClickListener {
 
 
     PlaceAnOrderViewModel placeAnOrderViewModel = null;
+    private TextToSpeech engine;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        engine = new TextToSpeech(getContext(), this);
     }
 
     private void init(View view){
@@ -84,6 +89,7 @@ public class BillingFragment extends Fragment implements View.OnClickListener {
 
         vOrderLogOut.setOnClickListener(this);
         vOrderSent.setOnClickListener(this);
+
     }
 
 
@@ -192,7 +198,18 @@ public class BillingFragment extends Fragment implements View.OnClickListener {
     }
 
 
+    @Override
+    public void onInit(int i) {
+        if (i == TextToSpeech.SUCCESS) {
+            engine.setLanguage(Locale.UK);
+            speech(getContext().getString(R.string.close_order_voice));
+        }
 
+    }
+
+    private void speech(String textToSpeech) {
+        engine.speak(textToSpeech, TextToSpeech.QUEUE_FLUSH, null, null);
+    }
 
 
 
